bd44bdca2e18509be02dfc370e3927bc1d2663bb
Pseurae <adhith.chand@gmail.com>
2023-08-06 22:16:17 +0530

    Start of dev

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,8 +1,8 @@
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 
-#include "imgui.h"
 #define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui.h"
 #include "imgui_internal.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
